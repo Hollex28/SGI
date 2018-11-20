01ae37f48223d8992e5315b628b7dfc7e3d6c800
TENDERO LARA <adtenla@dsic.upv.es>
2018-11-20 21:01:40 +0100

    Practica 5 acabada (creo) falta limpiar directorios

--- Proyectos/Practica 5/Practica 5/Reloj.cpp
+++ Proyectos/Practica 5/Practica 5/Reloj.cpp
@@ -19,7 +19,11 @@
 using namespace std;
 
 // Variables dependientes del tiempo
-static float alfa = 0;
+static float segundo;
+static float minuto;
+static float hora;
+static float alpha;
+
 
 void init()
 {
@@ -63,61 +67,81 @@ void display()
 	glLoadIdentity();
 
 	// Situar y orientar la camara: punto de vista, punto de interes, vertical
-	gluLookAt(3, 3, 4, 0, 0, 0, 0, 1, 0);
+	gluLookAt(0, 0, 7, 0, 0, 0, 0, 1, 0);
 
-	ejes();
-	static GLuint horas, centro;
-	// Marcadores de las horas
+	//ejes();
+	static GLuint horas, Marcadores;
+	// Marcador de hora
 	horas = glGenLists(1);
 	glNewList(horas, GL_COMPILE);
 
 	glColor3f(1.0, 1.0, 1.0);
 	glPushMatrix();
-	glScalef(0.15, 0.5, 0.15);
+	glScalef(0.15, 0.25, 0.15);
 	glutSolidSphere(1, 20, 20);
 	glPopMatrix();
 	glEndList();
 
-	// Centro del reloj
-	centro = glGenLists(1);
-	glNewList(centro, GL_COMPILE);
+
+	// Marcadores de hora
+	Marcadores = glGenLists(1);
+	glNewList(Marcadores, GL_COMPILE);
 
 	for (auto i = 0; i < 12; i++) {
-		/// el petalo en la corola
+		
 		glPushMatrix();
 		glRotatef(i * 30, 0, 0, 1);
 		glTranslatef(00, -1, 0.0);
-		glScalef(0.5, 0.5, 0.5);
-		glCallList(horas);
+		glScalef(0.25, 0.25, 0.25);
+		glRotatef(alpha, 0, 1, 0);
+		glutSolidTeapot(.5);
 		glPopMatrix();
 	}
 
-	glColor3f(1, 1, 0);
-	glPushMatrix();
-	glScalef(0.2, 0.2, 0.2);
-	glutSolidSphere(0.5, 20, 20);
 	glPopMatrix();
 
 	glEndList();
-	
+
 	
 	glPushMatrix();
 	glTranslatef(0, 0, 0);
 	glColor3f(0, 1, 0);
 	glRotatef(0, 0, 1, 0);
-	glCallList(centro);
+	glCallList(Marcadores);
 	glColor3f(1, 1, 1);
 	glPopMatrix();
-
+	//Segundero
 	glPushMatrix();
 	glTranslatef(0, 0, 0);
 	glColor3f(1, 0, 0);
 	glRotatef(-90, 1, 0, 0);
-	glRotatef(alfa / 60, 0, 1, 0);
+	glRotatef(segundo * 6, 0, 1, 0);
 	glutSolidCone(0.05, 0.70, 10, 10);
 	glPopMatrix();
-	
+	// Minutero
+	glPushMatrix();
+	glTranslatef(0, 0, 0);
+	glColor3f(1, 0, 0);
+	glRotatef(-90, 1, 0, 0);
+	glRotatef(minuto * 6, 0, 1, 0);
+	glutSolidCone(0.05, 0.60, 10, 10);
+	glPopMatrix();
+	//Horero
+	glPushMatrix();
+	glTranslatef(0, 0, 0);
+	glColor3f(1, 0, 0);
+	glRotatef(-90, 1, 0, 0);
+	glRotatef(hora * 30, 0, 1, 0);
+	glutSolidCone(0.05, 0.40, 10, 10);
+	glPopMatrix();
+	//centro
+	glPushMatrix();
+	glTranslatef(0, 0, 0);
+	glColor3f(1, 0, 0);
+	glutSolidSphere(0.05, 20, 20);
+
 	glutSwapBuffers();
+
 	
 	FPS();
 }
@@ -142,33 +166,20 @@ void reshape(GLint w, GLint h)
 	// Camara perspectiva
 	gluPerspective(20, ra, 0.2, 10);
 }
-
+#pragma warning(disable : 4996)
 void update()
 {
 	// Fase de actualizacion
-
-	// Sin control del tiempo
-	//alfa += 0.1;
-
-	// Control del tiempo
-	static const float omega = 1;	// Vueltas/sg
-
-	// Hora anterior inicial
-	static int segundos = time(NULL) % 60;
-	static int antes = glutGet(GLUT_ELAPSED_TIME);
-	alfa = 6 * segundos;
-	// Hora actual
-	int ahora = glutGet(GLUT_ELAPSED_TIME);
-
-	// Tiempo transcurrido
-	float tiempo_transcurrido = (float)(ahora - antes) / 1000.0;
-
-	// incremento = velocidad * tiempo;
-	//alfa += 360 * omega * tiempo_transcurrido;
-
-	// Actualizar la hora para la proxima vez
-	antes = ahora;
-
+// Conseguir hora
+	time_t rawtime;
+	struct tm * timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	//sacar tiempos
+	segundo = timeinfo->tm_sec;
+	minuto = timeinfo->tm_min;
+	hora = timeinfo->tm_hour;
+	alpha++;
 	// Mandar evento de redibujo
 	glutPostRedisplay();
 
