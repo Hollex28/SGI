5fce2dd44963f4ad8a3b709fd777b753031aa976
Hollex28 <xdxdlol.tk@gmail.com>
2019-01-02 14:31:06 +0100

    Avanzes en la carretera con curvas dinamicas

--- Proyectos/PracticaVideojuego/PracticaVideojuego/Interfaz_de_Conduccion.cpp
+++ Proyectos/PracticaVideojuego/PracticaVideojuego/Interfaz_de_Conduccion.cpp
@@ -18,11 +18,16 @@ double static PosZ = 0;
 double static MirarX = 1;
 double static MirarZ = 0;
 double static last = 0;
+const float pi = 3.1415;
 
 bool static inicio = TRUE;
-/*double funcionCarretera(double x) {
-
-}*/
+double funcionCarretera(double x) {
+	
+	int A=5;
+	int T=10;
+	double res = A * sin(x * (2 * pi) / T);
+	return res;
+}
 void GirarIzquierda() {
 	if (alpha < 1) {
 		alpha += 0.01;
@@ -62,21 +67,38 @@ void Calculodeposicioncamara() {
 	glutSwapBuffers();
 
 }
+/*void GeneracionCircuito(int tamañoX) {
+	
+	double Zant = funcionCarretera( - 1);
+	double Zpost = funcionCarretera(tamañoX);
+	GLfloat v0[3] = { - 1,0,Zant + 5 / 2 }, v1[3] = { tamañoX,0,Zpost + 5 / 2 },
+		v2[3] = { tamañoX,0,Zpost - 5 / 2 }, v3[3] = {  -1,0,Zant - 5 / 2 };
+	GLfloat v4[3] = { -1,0,Zant + 5 / 2 }, v5[3] = { tamañoX,0,Zpost + 5 / 2 },
+		v6[3] = { tamañoX,0,Zpost - 5 / 2 }, v7[3] = { -1,0,Zant - 5 / 2 };
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glColor3f(1, 0.5, 0.5);
+	quad(v0, v3, v2, v1, 30, 20);
+	quad(v4, v7, v5, v6, 30, 20);
+	glutSwapBuffers();
+}
+*/
 void GeneracionCircuito(int tamañoX) {
-	int i = PosX;
+	double i = PosX;
 	double static FinQuak;
 	if (inicio) { FinQuak = -10000; }
-	GLfloat v0[3] = { i - 1,0,5 }, v1[3] = { i + tamañoX,0,5 }, v2[3] = { i + tamañoX,0,-5 }, v3[3] = { i - 1,0,-5 };
+	double Zant = funcionCarretera(i - 1);
+	double Zpost = funcionCarretera(i + tamañoX);
+	GLfloat v0[3] = { i - 1,0,Zant+5/2 }, v1[3] = { i + tamañoX,0,Zpost+5/2 },
+			v2[3] = { i + tamañoX,0,Zpost-5/2 }, v3[3] = { i - 1,0,Zant-5/2 };
 	glPolygonMode(GL_FRONT, GL_LINE);
 	glColor3f(1, 0.5, 0.5);
 	quad(v0, v3, v2, v1, 30, 20);
 	
-	if (i>=FinQuak-20 || i == 0) {
+	if (i>=FinQuak-10 || i == 0) {//La condicion 1<=FinQuak-20 el -20 se saca del campo de vision que hay puesto en el reshape
 		FinQuak = i + tamañoX;
 		glutSwapBuffers();
 		cout << "genero, POSX = " << i << "Fin de Quak = " << FinQuak<<"\n";
 	}
-
 }
 void init()
 //Inicializaciones
@@ -93,7 +115,7 @@ void onDisplay()
 	glLoadIdentity();
 	if (inicio){
 		gluLookAt(0, 1, 0, 1, 1, 0, 0, 1, 0);
-		GeneracionCircuito(50);
+		GeneracionCircuito(10);
 	}
 	else {
 		/*double static PosX = 0;
@@ -101,7 +123,7 @@ void onDisplay()
 		double static MirarX = 1;
 		double static MirarZ = 0;*/
 		gluLookAt(PosX, 1, PosZ, MirarX, 1, MirarZ, 0, 1, 0); // Posiciona la camara
-		GeneracionCircuito(50);
+		GeneracionCircuito(10);
 	}
 
 		
@@ -144,7 +166,7 @@ void onReshape(int w, int h)
 	glLoadIdentity();
 	float razon = (float)w / h;
 	/* CAMARA PERSPECTIVA */
-	gluPerspective(45, razon, 1, 20);
+	gluPerspective(45, razon, 1, 10);
 }
 void update()
 {
